79663a9f26f9a6348207359f946be242fab3fe28
Anders Pearson <anders@columbia.edu>
2013-11-25 14:18:23 -0500

    add some view tests

--- polarexplorer/main/tests/test_views.py
+++ polarexplorer/main/tests/test_views.py
@@ -14,3 +14,27 @@ class BasicViewTest(TestCase):
         response = self.c.get("/smoketest/")
         self.assertEquals(response.status_code, 200)
         assert "PASS" in response.content
+
+    def test_glacier(self):
+        response = self.c.get("/glacier/")
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('main/glacier.html'
+                        in [t.name for t in response.templates])
+
+    def test_water(self):
+        response = self.c.get("/water/")
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('main/water.html'
+                        in [t.name for t in response.templates])
+
+    def test_gallery(self):
+        response = self.c.get("/gallery/antarctica/")
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('main/gallery.html'
+                        in [t.name for t in response.templates])
+
+    def test_gallery_nonexistant(self):
+        response = self.c.get("/gallery/doesnotexist/")
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('main/gallery.html'
+                        in [t.name for t in response.templates])
