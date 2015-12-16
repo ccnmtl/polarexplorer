dc61e0486fea611559b513878721806e8e805354
Anders Pearson <anders@columbia.edu>
2015-12-16 16:41:25 +0000

    convert to ccnmtlsettings

--- polarexplorer/settings_production.py
+++ polarexplorer/settings_production.py
@@ -1,49 +1,14 @@
 # flake8: noqa
 from settings_shared import *
-import os
+from ccnmtlsettings.production import common
 
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-MEDIA_ROOT = '/var/www/polarexplorer/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/polarexplorer/polarexplorer/sitemedia'),
-)
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'polarexplorer',
-        'HOST': '',
-        'PORT': 6432,
-        'USER': '',
-        'PASSWORD': '',
-    }
-}
-
-COMPRESS_ROOT = "/var/www/polarexplorer/polarexplorer/media/"
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
-AWS_STORAGE_BUCKET_NAME = "ccnmtl-polarexplorer-static-prod"
-AWS_PRELOAD_METADATA = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
-S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
-COMPRESS_ROOT = STATIC_ROOT
-COMPRESS_URL = STATIC_URL
-DEFAULT_FILE_STORAGE = 'cacheds3storage.MediaRootS3BotoStorage'
-MEDIA_URL = S3_URL + '/media/'
-COMPRESS_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
-AWS_QUERYSTRING_AUTH = False
-
-if 'migrate' not in sys.argv:
-    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+locals().update(
+    common(
+        project=project,
+        base=base,
+        STATIC_ROOT=STATIC_ROOT,
+        INSTALLED_APPS=INSTALLED_APPS,
+    ))
 
 try:
     from local_settings import *
--- polarexplorer/settings_shared.py
+++ polarexplorer/settings_shared.py
@@ -1,150 +1,30 @@
 # Django settings for polarexplorer project.
 import os.path
-import sys
+from ccnmtlsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+project = 'polarexplorer'
+base = os.path.dirname(__file__)
+locals().update(common(project=project, base=base))
 
-ADMINS = ()
-
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'polarexplorer',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-        'ATOMIC_REQUESTS': True,
-    }
-}
-
-if 'test' in sys.argv or 'jenkins' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '',
-            'ATOMIC_REQUESTS': True,
-        }
-    }
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-)
 PROJECT_APPS = [
     'polarexplorer.main',
 ]
 
-ALLOWED_HOSTS = ['localhost', '.ccnmtl.columbia.edu']
-
 USE_TZ = True
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/polarexplorer/uploads/"
-MEDIA_URL = '/uploads/'
-STATIC_URL = '/media/'
-SECRET_KEY = ')ng#)ef_u@_^zvvu@dxm7ql-yb^_!a6%v3v^j3b(mp+)l+5%@h'
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.template.context_processors.debug',
-    'django.template.context_processors.request',
-    'django.template.context_processors.static',
-    'django.template.context_processors.media',
-    'stagingcontext.staging_processor',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'waffle.middleware.WaffleMiddleware',
-)
-
-ROOT_URLCONF = 'polarexplorer.urls'
-
-TEMPLATE_DIRS = (
-    "/var/www/polarexplorer/templates/",
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS += [  # noqa
     'sorl.thumbnail',
-    'django.contrib.admin',
     'tagging',
     'typogrify',
-    'compressor',
-    'django_statsd',
     'bootstrapform',
-    'waffle',
-    'django_jenkins',
-    'smoketest',
     'django_extensions',
     'polarexplorer.main',
-    'django_markwhat',
 ]
 
 LETTUCE_APPS = (
     'polarexplorer.main',
 )
 
-INTERNAL_IPS = ('127.0.0.1', )
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'polarexplorer'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
 THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[polarexplorer] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "polarexplorer@ccnmtl.columbia.edu"
-DEFAULT_FROM_EMAIL = SERVER_EMAIL
 
-STATICFILES_DIRS = ("media/",)
-STATIC_ROOT = "/tmp/polarexplorer/static"
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
 LOGIN_REDIRECT_URL = "/"
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
